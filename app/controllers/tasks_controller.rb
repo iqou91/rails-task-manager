2e91bd4ed5116ad5ccc935986cec2b0098aa4290
Iqbal Bashir <iqbal2bashir@gmail.com>
2023-11-14 16:16:21 +0100

    TasksCompleted

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+# TasksController handles the CRUD operations for tasks.
+# It includes actions for listing tasks, showing a single task,
+# creating a new task, updating an existing task, and deleting a task.
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # No need for app/views/tasks/create.html.erb
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
